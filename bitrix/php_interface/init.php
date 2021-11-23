5b2c17ddf3b9fe53dcd6e50ee1b017307ad3c95c
cerealwhay <cerealwhay@gmail.com>
2021-11-23 12:46:33 +0300

    changed init.php

--- bitrix/php_interface/init.php
+++ bitrix/php_interface/init.php
@@ -1,4 +1,10 @@
-f (file_exists($_SERVER["DOCUMENT_ROOT"]."/bitrix/vendor/autoload.php")) {
-    require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/vendor/autoload.php");
+<?
+if (file_exists($_SERVER["DOCUMENT_ROOT"]."/local/vendor/autoload.php")) {
+    require_once($_SERVER["DOCUMENT_ROOT"] . "/local/vendor/autoload.php");
 }
 
+if (file_exists($_SERVER["DOCUMENT_ROOT"]."bitrix/php_interface/include/mysclad-api/product_parser.php")) {
+    require_once($_SERVER["DOCUMENT_ROOT"] . "bitrix/php_interface/include/mysclad-api/product_parser.php");
+}
+
+?>
